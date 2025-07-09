8ff7fa56de25c7fbec211cebaf82c06ade72304c
Mario <mzamora114@hotmail.com>
2025-07-08 20:27:00 -0600

    Commit del 08/07/2025

--- src/com/fibonacci/NumerosFibonacci.java
+++ src/com/fibonacci/NumerosFibonacci.java
@@ -24,7 +24,7 @@ public class NumerosFibonacci {
 		
 		int a=0,b=1, cont=0, c=0;
 		List<Integer> fibo = new ArrayList<>();
-		System.out.println("NUEVO COMMIT DEL --> 05/07/2025");						
+		System.out.println("NUEVO COMMIT CON FECHA --> 08/07/2025");						
 		Scanner longitudTerminos = new Scanner(System.in);		
 		System.out.println("Ingresa el total de terminos de la serie Fibonacci => ");
 		c = longitudTerminos.nextInt();
